bf8b433f9e0278b466b114e4fc4b6220568c4eb1
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-08-23 11:41:11 +0300

    toimii

--- App.js
+++ App.js
@@ -5,68 +5,68 @@ export default function App() {
 
   const [eka, setEka] = useState(0);
   const [toka, setToka] = useState(0);
-  const [tulos, setTulos] = useState(0);
+  const [tulos, setTulos] = useState('');
 
- 
+  const addition = () => {
+    setTulos(parseInt(eka) + parseInt(toka));
+  }
+
+  const subtraction = () => {
+    setTulos(eka - toka);
+  }
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={styles.container}>
         <View>
-          <Text>Tulos: </Text>
-          <TextInput
+          <Text>Tulos: {tulos}</Text>
+          
+          <TextInput 
+            placeholder='first number'
             keyboardType='numeric'
-            placeholder='Ensimmäinen numero'
             onChangeText={n => setEka(n)}
-            value={eka}
           />
           <TextInput 
+            placeholder='second number'
             keyboardType='numeric'
-            placeholder='Toinen numero'
             onChangeText={n => setToka(n)}
           />
-        
-      
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-          
-          
-          <Pressable
-          style={({ pressed }) => [
-            styles.button
-          ]}
-          >
-            <Text style={styles.buttonText}>+</Text>
-          </Pressable>
-
-          <Pressable 
-          style={({ pressed }) => [
-            styles.button
-          ]}>
-            <Text style={styles.buttonText}>-</Text>
-          </Pressable>
-
-        </View>
       </View>
+
+      <View style={styles.button}>
+
+        <Pressable 
+          style={({ pressed }) => styles.button}
+          onPress={addition}
+          >
+          <Text>+</Text>
+        </Pressable>
+
+        <Pressable 
+          style={({ pressed }) => styles.button}
+          onPress={subtraction}  
+        >
+          <Text>-</Text>
+        </Pressable>
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }, 
   button: {
+    flexDirection: 'row',
     backgroundColor: '#159fcf',
     paddingHorizontal: 10,
     paddingVertical: 6,
-    borderRadius: 3
+    borderRadius: 3,
   },
-  buttonText: {
-    color: 'white'
-  }
 })
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
