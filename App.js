dace528028a91fe4471d4988d3d59aed27bf1ca2
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-08-23 11:18:32 +0300

    add to github

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [eka, setEka] = useState(0);
+  const [toka, setToka] = useState(0);
+  const [tulos, setTulos] = useState(0);
+
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View>
+          <Text>Tulos: </Text>
+          <TextInput
+            keyboardType='numeric'
+            placeholder='Ensimmäinen numero'
+            onChangeText={n => setEka(n)}
+            value={eka}
+          />
+          <TextInput 
+            keyboardType='numeric'
+            placeholder='Toinen numero'
+            onChangeText={n => setToka(n)}
+          />
+        
+      
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          
+          
+          <Pressable
+          style={({ pressed }) => [
+            styles.button
+          ]}
+          >
+            <Text style={styles.buttonText}>+</Text>
+          </Pressable>
+
+          <Pressable 
+          style={({ pressed }) => [
+            styles.button
+          ]}>
+            <Text style={styles.buttonText}>-</Text>
+          </Pressable>
+
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  button: {
+    backgroundColor: '#159fcf',
+    paddingHorizontal: 10,
+    paddingVertical: 6,
+    borderRadius: 3
   },
-});
+  buttonText: {
+    color: 'white'
+  }
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
